b3a88e58a41d12bb6705596229d59342fd9af1aa
Saishree Jayakumar <saishreejayakumar@Saishrees-MacBook-Pro.local>
2020-04-03 21:20:16 -0700

    Location Permission Setting

--- app/src/main/java/com/example/locatespot/MainActivity.java
+++ app/src/main/java/com/example/locatespot/MainActivity.java
@@ -1,45 +1,40 @@
 package com.example.locatespot;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
-import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Looper;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
 import java.io.IOException;
@@ -50,8 +45,8 @@ import java.util.Set;
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private int locationRequestCode = 1000;
-    private FusedLocationProviderClient mFusedLocationClient;
+    private int permissionCode = 500;
+    private FusedLocationProviderClient locationClient;
     private EditText addr;
     private GoogleMap map;
     private RecyclerView recyclerView;
@@ -59,6 +54,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     private Set<String> alreadyAvailableAddressLookUp;
     private RecyclerViewAdapter recyclerViewAdapter;
     private AlertDialog invalidAddressAlert;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,14 +66,42 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         alreadyAvailableAddress = new ArrayList<>();
         alreadyAvailableAddressLookUp = new HashSet<>();
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
-                    locationRequestCode);
+        if (isUserPermissionAvailable()) {
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+                    || locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                locationClient = LocationServices.getFusedLocationProviderClient(this);
+                ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps)).getMapAsync(this);
+
+            } else {
+                Intent providerSetting = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(providerSetting);
+            }
+
         } else {
-            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-            ((SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.maps)).getMapAsync(this);
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION}, permissionCode);
         }
+    }
+
+    private boolean isUserPermissionAvailable() {
+        if ((ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) ||
+                (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (permissionCode == requestCode && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            locationClient = LocationServices.getFusedLocationProviderClient(this);
+            ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps)).getMapAsync(this);
+        } else {
+            finish();
+        }
+
     }
 
     public void findAddress(View view) {
@@ -91,19 +115,19 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         recyclerViewAdapter = new RecyclerViewAdapter(getApplicationContext(), alreadyAvailableAddress, map);
         recyclerView.setAdapter(recyclerViewAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mFusedLocationClient.getLastLocation().addOnCompleteListener(
+        locationClient.getLastLocation().addOnCompleteListener(
                 new OnCompleteListener<Location>() {
                     @Override
                     public void onComplete(@NonNull Task<Location> task) {
                         final Location location = task.getResult();
                         if (location == null) {
-                            LocationRequest mLocationRequest = new LocationRequest();
-                            mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-                            mLocationRequest.setInterval(0);
-                            mLocationRequest.setFastestInterval(0);
-                            mLocationRequest.setNumUpdates(1);
-                            mFusedLocationClient.requestLocationUpdates(
-                                    mLocationRequest,
+                            LocationRequest locRequest = new LocationRequest();
+                            locRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                            locRequest.setInterval(5);
+                            locRequest.setFastestInterval(0);
+                            locRequest.setNumUpdates(1);
+                            locationClient.requestLocationUpdates(
+                                    locRequest,
                                     new LocationCallback() {
                                         @Override
                                         public void onLocationResult(LocationResult locationResult) {
@@ -125,7 +149,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         map.clear();
         LatLng currentLocation = new LatLng(latitude, longitude);
         map.addMarker(new MarkerOptions().position(currentLocation).title(loc == null ? "Your Location" : loc));
-        map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15), 3500, null);
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 16), 3500, null);
     }
 
     public void findAddress(String addressVal) {
