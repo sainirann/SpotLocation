50749d8603501febc6ebdccd78d2cfae4559966b
santhoshkumarml <santhosh@streamsets.com>
2020-04-19 21:13:19 -0700

    Fix Address line in the recyler

--- app/src/main/java/com/example/locatespot/MainActivity.java
+++ app/src/main/java/com/example/locatespot/MainActivity.java
@@ -1,11 +1,5 @@
 package com.example.locatespot;
 
-import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.FragmentActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Context;
@@ -24,8 +18,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ZoomControls;
-
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
@@ -50,171 +47,181 @@ import java.util.Set;
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private int permissionCode = 500;
-    private FusedLocationProviderClient locationClient;
-    private EditText addr;
-    private GoogleMap map;
-    private static final String TAG = MainActivity.class.getSimpleName();
-    private RecyclerView recyclerView;
-    private List<RetrieveLocation> alreadyAvailableAddress;
-    private Set<String> alreadyAvailableAddressLookUp;
-    private RecyclerViewAdapter recyclerViewAdapter;
-    private AlertDialog invalidAddressAlert;
+  private int permissionCode = 500;
+  private FusedLocationProviderClient locationClient;
+  private EditText addr;
+  private GoogleMap map;
+  private static final String TAG = MainActivity.class.getSimpleName();
+  private RecyclerView recyclerView;
+  private List<RetrievedLocation> alreadyAvailableAddress;
+  private Set<String> alreadyAvailableAddressLookUp;
+  private RecyclerViewAdapter recyclerViewAdapter;
+  private AlertDialog invalidAddressAlert;
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        recyclerView = findViewById(R.id.looked_up_address);
-        recyclerView.setVisibility(View.GONE);
-        alreadyAvailableAddress = new ArrayList<>();
-        Log.d(TAG, "In OnCreate");
-        alreadyAvailableAddressLookUp = new HashSet<>();
+    recyclerView = findViewById(R.id.looked_up_address);
+    recyclerView.setVisibility(View.GONE);
+    alreadyAvailableAddress = new ArrayList<>();
+    Log.d(TAG, "In OnCreate");
+    alreadyAvailableAddressLookUp = new HashSet<>();
 
 
 
-        if (isUserPermissionAvailable()) {
-            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-            if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
-                    || locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-                locationClient = LocationServices.getFusedLocationProviderClient(this);
-                ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps)).getMapAsync(this);
+    if (isUserPermissionAvailable()) {
+      LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+      if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+          || locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+        locationClient = LocationServices.getFusedLocationProviderClient(this);
+        ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps)).getMapAsync(this);
 
-            } else {
-                Intent providerSetting = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(providerSetting);
-            }
+      } else {
+        Intent providerSetting = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivity(providerSetting);
+      }
 
-        } else {
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_COARSE_LOCATION,
-                    Manifest.permission.ACCESS_FINE_LOCATION}, permissionCode);
-        }
-
+    } else {
+      ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_COARSE_LOCATION,
+          Manifest.permission.ACCESS_FINE_LOCATION}, permissionCode);
     }
 
-    private boolean isUserPermissionAvailable() {
-        if ((ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) ||
-                (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
-            return false;
-        }
-        return true;
+  }
+
+  private boolean isUserPermissionAvailable() {
+    if ((ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) ||
+        (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    if (permissionCode == requestCode && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+      locationClient = LocationServices.getFusedLocationProviderClient(this);
+      ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps)).getMapAsync(this);
+    } else {
+      finish();
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (permissionCode == requestCode && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            locationClient = LocationServices.getFusedLocationProviderClient(this);
-            ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps)).getMapAsync(this);
-        } else {
-            finish();
-        }
+  }
 
+  public void findAddress(View view) {
+    addr = findViewById(R.id.address);
+    findAddress(addr.getText().toString());
+  }
+
+  @Override
+  public void onMapReady(final GoogleMap googleMap) {
+    map = googleMap;
+    map.setMyLocationEnabled(true);
+    UiSettings uiToolFeatures = map.getUiSettings();
+    uiToolFeatures.setZoomControlsEnabled(true);
+    uiToolFeatures.setMapToolbarEnabled(true);
+    uiToolFeatures.setZoomGesturesEnabled(true);
+    uiToolFeatures.setTiltGesturesEnabled(true);
+    uiToolFeatures.setRotateGesturesEnabled(true);
+    try {
+      boolean result = map.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.map_design));
+      if (!result) {
+        Log.e(TAG, "Style parsing failed.");
+      }
+    } catch (Resources.NotFoundException e) {
+      Log.e(TAG, "Can't find style. Error: ", e);
     }
-
-    public void findAddress(View view) {
-        addr = findViewById(R.id.address);
-        findAddress(addr.getText().toString());
-    }
-
-    @Override
-    public void onMapReady(final GoogleMap googleMap) {
-        map = googleMap;
-        map.setMyLocationEnabled(true);
-        UiSettings uiToolFeatures = map.getUiSettings();
-        uiToolFeatures.setZoomControlsEnabled(true);
-        uiToolFeatures.setMapToolbarEnabled(true);
-        uiToolFeatures.setZoomGesturesEnabled(true);
-        uiToolFeatures.setTiltGesturesEnabled(true);
-        uiToolFeatures.setRotateGesturesEnabled(true);
-        try {
-            boolean result = map.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.map_design));
-            if (!result) {
-                Log.e(TAG, "Style parsing failed.");
-            }
-        } catch (Resources.NotFoundException e) {
-            Log.e(TAG, "Can't find style. Error: ", e);
-        }
-        recyclerViewAdapter = new RecyclerViewAdapter(getApplicationContext(), alreadyAvailableAddress, map);
-        recyclerView.setAdapter(recyclerViewAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        locationClient.getLastLocation().addOnCompleteListener(
-                new OnCompleteListener<Location>() {
+    recyclerViewAdapter = new RecyclerViewAdapter(getApplicationContext(), alreadyAvailableAddress, map);
+    recyclerView.setAdapter(recyclerViewAdapter);
+    recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    locationClient.getLastLocation().addOnCompleteListener(
+        new OnCompleteListener<Location>() {
+          @Override
+          public void onComplete(@NonNull Task<Location> task) {
+            final Location location = task.getResult();
+            if (location == null) {
+              LocationRequest locRequest = new LocationRequest();
+              locRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+              locRequest.setInterval(5);
+              locRequest.setFastestInterval(0);
+              locRequest.setNumUpdates(1);
+              locationClient.requestLocationUpdates(
+                  locRequest,
+                  new LocationCallback() {
                     @Override
-                    public void onComplete(@NonNull Task<Location> task) {
-                        final Location location = task.getResult();
-                        if (location == null) {
-                            LocationRequest locRequest = new LocationRequest();
-                            locRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-                            locRequest.setInterval(5);
-                            locRequest.setFastestInterval(0);
-                            locRequest.setNumUpdates(1);
-                            locationClient.requestLocationUpdates(
-                                    locRequest,
-                                    new LocationCallback() {
-                                        @Override
-                                        public void onLocationResult(LocationResult locationResult) {
-                                            Location loc = locationResult.getLastLocation();
-                                            setCurrentLocation(loc.getLatitude(), loc.getLongitude(), null);
-                                        }
-                                    },
-                                    Looper.myLooper()
-                            );
-                        } else {
-                            setCurrentLocation(location.getLatitude(), location.getLongitude(), null);
-                        }
+                    public void onLocationResult(LocationResult locationResult) {
+                      Location loc = locationResult.getLastLocation();
+                      setCurrentLocation(loc.getLatitude(), loc.getLongitude(), null);
                     }
-                }
-        );
-    }
-
-    private void setCurrentLocation(double latitude, double longitude, String loc) {
-        map.clear();
-        LatLng currentLocation = new LatLng(latitude, longitude);
-        map.addMarker(new MarkerOptions().position(currentLocation).title(loc == null ? "Your Location" : loc));
-        map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 16), 3500, null);
-    }
-
-    public void findAddress(String addressVal) {
-        Geocoder gc = new Geocoder(this);
-        if(gc.isPresent()){
-            List<Address> availableAddress = new ArrayList<>();
-
-            try {
-                availableAddress = gc.getFromLocationName(addressVal, 1);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            if (availableAddress.size() > 0) {
-                recyclerView.setVisibility(View.VISIBLE);
-                Address address = availableAddress.get(0);
-                recyclerView.setAlpha(1);
-                RetrieveLocation r = new RetrieveLocation(address, addressVal);
-                if (!alreadyAvailableAddressLookUp.contains(addressVal)) {
-                    alreadyAvailableAddress.add(r);
-                    alreadyAvailableAddressLookUp.add(addressVal);
-                    recyclerViewAdapter.notifyDataSetChanged();
-                }
-                setCurrentLocation(address.getLatitude(), address.getLongitude(), addressVal);
-                addr.setText("");
+                  },
+                  Looper.myLooper()
+              );
             } else {
-                invalidAddressAlert = new AlertDialog.Builder(this).create();
-                View view = LayoutInflater.from(getApplicationContext()).inflate(R.layout.alert_box_layout,null);
-                Button alertButton = view.findViewById(R.id.alert_try_agin);
-                alertButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        addr.setText("");
-                        map.clear();
-                        invalidAddressAlert.dismiss();
-                    }
-                });
-                invalidAddressAlert.setView(view);
-                invalidAddressAlert.show();
+              setCurrentLocation(location.getLatitude(), location.getLongitude(), null);
             }
+          }
         }
+    );
+  }
+
+  private void setCurrentLocation(double latitude, double longitude, String loc) {
+    map.clear();
+    LatLng currentLocation = new LatLng(latitude, longitude);
+    map.addMarker(new MarkerOptions().position(currentLocation).title(loc == null ? "Your Location" : loc));
+    map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 16), 3500, null);
+  }
+
+  private void findAddress(String addressVal) {
+    Geocoder gc = new Geocoder(this);
+    if (Geocoder.isPresent()){
+      List<Address> availableAddress;
+      try {
+        availableAddress = gc.getFromLocationName(addressVal, 1);
+      } catch (IOException e) {
+        Log.e(getClass().getCanonicalName(), e.getMessage());
+        return;
+      }
+      if (!availableAddress.isEmpty()) {
+        recyclerView.setVisibility(View.VISIBLE);
+        Address address = availableAddress.get(0);
+        recyclerView.setAlpha(1);
+        RetrievedLocation r = new RetrievedLocation(address, getAddressLines(address));
+        if (!alreadyAvailableAddressLookUp.contains(addressVal)) {
+          //Always add recent one first
+          alreadyAvailableAddress.add(0, r);
+          alreadyAvailableAddressLookUp.add(addressVal);
+          recyclerViewAdapter.notifyDataSetChanged();
+        }
+        setCurrentLocation(address.getLatitude(), address.getLongitude(), addressVal);
+        addr.setText("");
+      } else {
+        invalidAddressAlert = new AlertDialog.Builder(this).create();
+        View view = LayoutInflater.from(getApplicationContext()).inflate(R.layout.alert_box_layout,null);
+        Button alertButton = view.findViewById(R.id.alert_try_agin);
+        alertButton.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            addr.setText("");
+            map.clear();
+            invalidAddressAlert.dismiss();
+          }
+        });
+        invalidAddressAlert.setView(view);
+        invalidAddressAlert.show();
+      }
     }
+  }
+
+
+  private String getAddressLines(Address address) {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
+      sb.append(address.getAddressLine(i));
+    }
+    return sb.toString();
+  }
 
 }
--- app/src/main/java/com/example/locatespot/RetrievedLocation.java
+++ app/src/main/java/com/example/locatespot/RetrievedLocation.java
@@ -2,10 +2,11 @@ package com.example.locatespot;
 
 import android.location.Address;
 
-public class RetrieveLocation {
+public class RetrievedLocation {
     Address address;
     String completeAddress;
-    public RetrieveLocation(Address address, String completeAddress) {
+
+    RetrievedLocation(Address address, String completeAddress) {
         this.address = address;
         this.completeAddress = completeAddress;
     }
